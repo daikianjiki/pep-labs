ed3e39ec8ff7879c48bd1186843809ec3e7c17ec
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-04 21:04:39 +0000

    Saved progress on Queue 2023-01-04 21:04:39 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -14,9 +14,15 @@ public class DoesNotContain {
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
         for (int i = 0; i < arr.length; i++) {
-            if (!(arr[i] == target)) {
+            // if (arr[i] != target) {
+            //     return true;
+            // } 
+            // while (arr[i] != target) {
+            //     return true;
+            // }
+            do {
                 return true;
-            }
+            } while (arr[i] != target);
         } return false;
     }
 }
