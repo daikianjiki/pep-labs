82dc416ae2b26dd10802a6742711b8340616bc2e
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-09 20:03:00 +0000

    Saved progress on Contains 2023-01-09 20:03:00 +0000

--- GroceryList/src/main/java/GroceryDAO.java
+++ GroceryList/src/main/java/GroceryDAO.java
@@ -28,7 +28,7 @@ public class GroceryDAO {
         List<String> groceries = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = SELECT * FROM groceries;
+            String sql = "SELECT * FROM Grocery";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -48,7 +48,7 @@ public class GroceryDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = ALTER TABLE groceries ADD groceryName varchar(255);
+            String sql = "INSERT INTO Grocery VALUES 'groceryName' " ;
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.executeUpdate();
         }catch(SQLException e){
