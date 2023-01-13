cc58df0cd14160910a2e9d5de3ed5bbcf961cfc8
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-13 20:55:44 +0000

    Saved progress on InnerJoin 2023-01-13 20:55:44 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,7 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
+        String
         for (int i = 0; i < n; i++) {
 
             for (int j = 0; j <= i; j++) {
