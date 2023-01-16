23f16fa33c690d579b18e506ec7e386a2691afee
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-16 21:08:50 +0000

    Saved progress on LargestSum 2023-01-16 21:08:50 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class LargestSum {
@@ -10,7 +11,14 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        if
-        return 0;
+        //index of the value should be differnt then each other.
+        //I can use nums.get(highestvalue1) and nums.get(highestvalue2)?
+        //Add those two numbers and return them.
+        //OR!!! Sort them so the last two numbers are the highest number. Add those two.
+        Collections.sort(nums);
+        int size = nums.size();
+        int firstNum = nums.get(size - 1);
+        int secondNum = nums.get(size - 2);
+        return firstNum+secondNum;
     }
 }
