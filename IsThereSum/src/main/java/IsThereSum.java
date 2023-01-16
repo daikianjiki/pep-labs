cfa77c0a16b93c4a95bf33178de6c900e61d4557
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-16 03:12:41 +0000

    Saved progress on IsThereSum 2023-01-16 03:12:41 +0000

--- IsThereSum/src/main/java/IsThereSum.java
+++ IsThereSum/src/main/java/IsThereSum.java
@@ -10,10 +10,12 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; i++) {
-                if (the sum of arr[i] and arr[j] matches target)
-                return true;
+        int size = arr.length;
+        for (int i = 0; i < (size - 1); i++) {
+            for (int j = (i + 1); j < size; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return true;
+                }
             }
         }
         return false;
