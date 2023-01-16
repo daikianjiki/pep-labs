4c31c18485177ba8399a8728de17688a105025cf
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-16 03:57:06 +0000

    Saved progress on Pyramid 2023-01-16 03:57:06 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,13 +27,15 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        String
+        String pyr = "";
+
         for (int i = 0; i < n; i++) {
 
             for (int j = 0; j <= i; j++) {
-                return "*";
+                pyr = pyr + "*";
             }
+            pyr = pyr + "\n";
         }
-        return "\n";
+        return pyr;
     }
 }
