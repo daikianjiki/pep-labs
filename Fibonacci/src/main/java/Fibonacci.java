e4ec2ca9d476908e1ac0bb80279394983f86a85f
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-17 18:04:28 +0000

    Saved progress on Fibonacci 2023-01-17 18:04:28 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,11 +15,14 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        if (n == 0 || n == 1) {
-            return 0;
-        } else if (n == 2) {
-            return 1;
-        } 
-        return fib(n-1) + fib(n-2);
+        int f[] = new int [n + 1];
+        f[0] = 0;
+        if (n > 0) {
+            f[1] = 1;
+            for (int i = 2; i <=n; i++) {
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+        return f[n];
     }
 }
