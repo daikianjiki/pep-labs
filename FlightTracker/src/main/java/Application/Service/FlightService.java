8235fba7f01c9b0f5d8ea8272cd6e8cf42e227cf
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-20 17:58:55 +0000

    Saved progress on RequestBody 2023-01-20 17:58:55 +0000

--- FlightTracker/src/main/java/Application/Service/FlightService.java
+++ FlightTracker/src/main/java/Application/Service/FlightService.java
@@ -61,7 +61,11 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return flightDAO.getFlightById(flight_id);
+        if (flightDAO.getFlightById(flight_id) != null) {
+            flightDAO.updateFlight(flight_id, flight);
+            return flightDAO.getFlightById(flight_id);
+        }
+        return null;
     }
 
     /**
