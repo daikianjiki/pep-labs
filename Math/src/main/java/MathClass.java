9b0252c0d7de30e547afbff1e92e276d44149b80
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-21 03:03:06 +0000

    Saved progress on RequestBody 2023-01-21 03:03:06 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -20,6 +20,6 @@ public class MathClass {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return Math.pow(a, b);
+        return (Math.pow(a, b));
     }
 }
