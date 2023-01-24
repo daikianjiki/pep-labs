451ee7ec5dfaec87b57b6c13c755dfdf55bb1493
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-24 21:49:26 +0000

    Saved progress on Isogram 2023-01-24 21:49:26 +0000

--- ArraysAreEqual/src/main/java/ArraysAreEqual.java
+++ ArraysAreEqual/src/main/java/ArraysAreEqual.java
@@ -17,7 +17,7 @@ public class ArraysAreEqual {
     public boolean equal(int[] a, int[] b){
         boolean isEqual = true;
         for (int i = 0; i < a.length; i++) {
-            if (a[i] == b[i]) {
+            if (a[i] != b[i]) {
                 isEqual = false;
             } 
         } return isEqual;
