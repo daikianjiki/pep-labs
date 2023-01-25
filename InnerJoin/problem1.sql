d76ed92a16257c71b3308919fbb934b63a90bd96
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-25 02:56:43 +0000

    Saved progress on Library 2023-01-25 02:56:43 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,1 +1,2 @@
-SELECT student.id, student.student_name FROM student INNER JOIN class ON class.class_title = student.class_title WHERE class.teacher_name = 'Ms. Lovelace';
+SELECT student.id, student.student_name FROM student INNER JOIN class 
+ON class.class_title = student.class_titleWHERE class.teacher_name = 'Ms. Lovelace';
