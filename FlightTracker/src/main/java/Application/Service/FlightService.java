1b41b1025832ee0c8ad362dfe38bb8f4488b9bf7
Daiki Anjiki <daikianjiki@gmail.com>
2023-01-31 01:42:16 +0000

    Saved progress on Library 2023-01-31 01:42:16 +0000

--- FlightTracker/src/main/java/Application/Service/FlightService.java
+++ FlightTracker/src/main/java/Application/Service/FlightService.java
@@ -61,11 +61,11 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        if (flightDAO.getFlightById(flight_id) != null) {
-            flightDAO.updateFlight(flight_id, flight);
-            return flightDAO.getFlightById(flight_id);
+        if (flightDAO.getFlightById(flight_id) == null) {
+            return null;
         }
-        return null;
+        flightDAO.updateFlight(flight_id, flight);
+        return flightDAO.getFlightById(flight_id);
     }
 
     /**
