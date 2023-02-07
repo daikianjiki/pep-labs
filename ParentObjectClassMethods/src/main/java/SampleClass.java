0b6c6a7a7d2e46014b48ad88e204b4b4a3674a19
Daiki Anjiki <daikianjiki@gmail.com>
2023-02-07 17:08:03 +0000

    Saved progress on OtherStringMethods 2023-02-07 17:08:02 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -72,5 +72,30 @@ public class SampleClass {
 
         return ab;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + a;
+        result = prime * result + (b ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SampleClass other = (SampleClass) obj;
+        if (a != other.a)
+            return false;
+        if (b != other.b)
+            return false;
+        return true;
+    }
     
 }
