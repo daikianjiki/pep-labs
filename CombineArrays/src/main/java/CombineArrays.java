7f1dbcd1e1936caa3350436c9cf7415e438d1bf3
Daiki Anjiki <daikianjiki@gmail.com>
2023-02-14 17:59:34 +0000

    Saved progress on PathParameters 2023-02-14 17:59:34 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -9,10 +9,23 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-        int arr3 = arr1.length + arr2.length;
-        int[] comArr = new int[arr3];
-        System.arraycopy(arr1, 0, comArr, 0, arr1.length);
-        System.arraycopy(arr2, 0, comArr, arr1.length, arr2.length);
-        return comArr;
+        int len = arr1.length + arr2.length;
+        // int[] comArr = new int[arr3];
+        // System.arraycopy(arr1, 0, comArr, 0, arr1.length);
+        // System.arraycopy(arr2, 0, comArr, arr1.length, arr2.length);
+        // return comArr;
+
+        // a way I can combine array without needing to use arraycopy.
+        int[] res = new int[len];
+        for (int i = 0, j = 0; i < len; i++) {
+            if (i < arr1.length) {
+                res[i] = arr1[i];
+            } else {
+                res[i] = arr2[j];
+                j++;
+            }
+        }
+        return res;
+
     }
 }
